b3f32056a95201ffe81189fb237236dc066c26ac
qlae <77710901+qlae@users.noreply.github.com>
2023-10-05 21:34:16 -0400

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,5 +1,18 @@
-/*
- * Implement your solution in thi file
- *
- */
+// lab1.c
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lab1.h"
+
+char* readString(const char* fileName) {
+    char* line = NULL;
+    size_t len = 0;
+    FILE* file = fopen(fileName, "r");
+    
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+}
